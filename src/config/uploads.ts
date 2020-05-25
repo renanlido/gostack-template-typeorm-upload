0e301b76d120678cdbb4b5d3708ab073c268ca8f
Renan Oliveira <renanlido@yahoo.com.br>
2020-05-25 08:42:06 -0300

    Feature - Solved exercice

---
+++ src/config/uploads.ts
@@ -0,0 +1,20 @@
+import multer from 'multer';
+import path from 'path';
+import crypto from 'crypto';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  directory: tmpFolder,
+
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename: (request, file, calback) => {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+
+      const filename = `${fileHash}-${file.originalname}`;
+
+      return calback(null, filename);
+    },
+  }),
+};
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,56 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({ title: category });
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (total < value && type === 'outcome') {
+      throw new AppError("You don't have balance");
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    delete transaction.category_id;
+
+    return transaction;
   }
 }
 
